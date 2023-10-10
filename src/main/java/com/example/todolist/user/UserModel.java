d3ef6cbdff8561e121e6c49330c9f4a467d8d891
Lucas Sales <lucassales398@gmail.com>
2023-10-10 12:22:39 -0300

    Add lombok, criação do user controller e model

---
+++ src/main/java/com/example/todolist/user/UserModel.java
@@ -0,0 +1,10 @@
+package com.example.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+}
